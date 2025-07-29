d4a635ae3e54aa69a859d14029cccfe0fec729b3
Ruppa Divya <divyaruppa001@gmail.com>
2025-07-29 13:46:38 +0530

    Add files via upload

---
+++ Bala.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.lang.String;
+import java.lang.System;
+class Bala
+{
+	public static void main(String[] args )
+	{
+		//Scanner sc = new Scanner (System.in);
+//		int p=10;
+//		for(int i=10;i<=15;i++)
+//			{
+//				System.out.println(p);
+//				i=5;
+//				i++;
+//			System.out.println(i);
+//
+//			}
+			
+			
+			
+			
+			//while (10<20)
+			//	{
+			//		int p=5;
+			//		System.out.println(p);
+			//		p++;
+			//		p=p+3;
+			//		System.out.println(p);
+			//	}
+			
+			
+			
+	int k=5,p=7;
+	for (int i=5;k<15;p++)
+		{
+			System.out.println(i+" "+k+" "+p);
+			k++;
+			p=k+p;
+			System.out.println("p value is :");
+			System.out.println(p+" "+k);
+		}
+	//
+	
+	//int m=10;
+	//for (int i=1;i<7;m++)
+	//	{
+	//		System.out.println(m);
+	//		if (m==13)
+	//			System.out.println(m);
+	//		    m++;
+	//		    System.out.println(i);
+	//	}
+	}
+}
